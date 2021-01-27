b3f020e70f1816cbc7c9c9b8eaa369b8ad284b1f
jaredbest <me@jaredbest.com>
2021-01-27 08:58:58 +0100

    Updated project packages and completed training

--- Assets/Scripts/KartAgent.cs
+++ Assets/Scripts/KartAgent.cs
@@ -7,41 +7,51 @@ using UnityEngine;
 
 public class KartAgent : Agent
 {
-   public CheckpointManager _checkpointManager;
-   private KartController _kartController;
-   
-   //called once at the start
-   public override void Initialize()
-   {
-      _kartController = GetComponent<KartController>();
-   }
-   
-   //Called each time it has timed-out or has reached the goal
-   public override void OnEpisodeBegin()
-   {
-      _checkpointManager.ResetCheckpoints();
-      _kartController.Respawn();
-   }
+    public CheckpointManager _checkpointManager;
+    private KartController _kartController;
 
-   #region Edit this region!
+    //called once at the start
+    public override void Initialize()
+    {
+        _kartController = GetComponent<KartController>();
+    }
 
-      //Collecting extra Information that isn't picked up by the RaycastSensors
-      public override void CollectObservations(VectorSensor sensor)
-      {
-         
-      }
+    //Called each time it has timed-out or has reached the goal
+    public override void OnEpisodeBegin()
+    {
+        _checkpointManager.ResetCheckpoints();
+        _kartController.Respawn();
+    }
 
-      //Processing the actions received
-      public override void OnActionReceived(ActionBuffers actions)
-      {
-         
-      }
-      
-      //For manual testing with human input, the actionsOut defined here will be sent to OnActionRecieved
-      public override void Heuristic(in ActionBuffers actionsOut)
-      {
-         
-      }
-      
-   #endregion
+    #region Edit this region!
+
+    //Collecting extra Information that isn't picked up by the RaycastSensors
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        // Vector between Kart and next checkpoint
+        Vector3 diff = _checkpointManager.nextCheckPointToReach.transform.position - transform.position;
+        sensor.AddObservation(diff / 20f); // Divide by 20 to normalize
+        
+        AddReward(-0.001f); // Promote faster driving
+    }
+
+        //Processing the actions received
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        var input = actions.ContinuousActions;
+
+        _kartController.ApplyAcceleration(input[1]);
+        _kartController.Steer(input[0]);
+    }
+
+    //For manual testing with human input, the actionsOut defined here will be sent to OnActionReceived
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        var action = actionsOut.ContinuousActions;
+
+        action[0] = Input.GetAxis("Horizontal"); // Steering
+        action[1] = Input.GetKey(KeyCode.W) ? 1f : 0f; // Acceleration
+    }
+
+    #endregion
 }
